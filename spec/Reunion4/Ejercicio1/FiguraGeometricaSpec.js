2c68b6a9db684c7b19f28cabf8c3e4da0779fc08
Osukaru <zepolracso@gmail.com>
2011-08-23 18:19:43 +0200

    Descripcion básica de FiguraGeometrica

--- spec/Reunion4/Ejercicio1/FiguraGeometricaSpec.js
+++ spec/Reunion4/Ejercicio1/FiguraGeometricaSpec.js
@@ -5,11 +5,23 @@ describe("FiguraGeometrica", function() {
     figuraGeometrica = new FiguraGeometrica();
   });
 
-  it("tiene un metodo para calcular el area", function() {
+  it("tiene definido un metodo para calcular el area", function() {
     expect("calculaArea" in figuraGeometrica).toBeTruthy()
   });
 
-  it("tiene un metodo para calcular el perimetro", function() {
+  it("tiene definido un metodo para calcular el perimetro", function() {
     expect("calculaPerimetro" in figuraGeometrica).toBeTruthy()
   });
+
+  it("tiene definido un metodo para obtener la posicion", function() {
+  	expect("getPosicion" in figuraGeometrica).toBeTruthy()
+  }); 	
+
+  it("tiene definido un metodo para obtener las dimensiones", function() {
+  	expect("getDimensiones" in figuraGeometrica).toBeTruthy()
+  }); 	
+
+  it("tiene definido un metodo para describir el objeto por consola", function() {
+  	expect("describe" in figuraGeometrica).toBeTruthy()
+  }); 	
 });
--- src/Reunion4/Ejercicio1/FiguraGeometrica.js
+++ src/Reunion4/Ejercicio1/FiguraGeometrica.js
@@ -1,9 +1,23 @@
 function FiguraGeometrica() {
+	var posicion, dimensiones;
 }
 
 FiguraGeometrica.prototype.calculaArea = function() {
-	return null;
+	return undefined;
 };
+
 FiguraGeometrica.prototype.calculaPerimetro = function() {
-	return null;
+	return undefined;
+};
+
+FiguraGeometrica.prototype.getPosicion = function() {
+	return posicion;
+};
+
+FiguraGeometrica.prototype.getDimensiones = function() {
+	return dimensiones;
+};
+
+FiguraGeometrica.prototype.describe = function() {
+	console.log(undefined);
 };
